0ae005050c72ee9a5be9ec00e857892cc1f9f127
YasugiMomo <yasugimomo.crw@gmail.com>
2022-05-28 16:14:25 +0000

    Edit image_tag

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -6,7 +6,7 @@ class Public::CommentsController < ApplicationController
     comment.review_id = review.id
     if comment.save
       flash[:notice] = "コメントを投稿しました。"
-      # redirect_to shop_review_path(review.shop_id, review.id)
+      redirect_to shop_review_path(review.shop_id, review.id)
       # app/views/public/comments/create.js.erbを参照する
     else
       flash[:alert] = "コメントの投稿に失敗しました。入力内容をご確認いただき、再度お試しください。"
@@ -19,7 +19,7 @@ class Public::CommentsController < ApplicationController
     Comment.find(params[:id]).destroy
     flash[:notice] = "コメントを削除しました。"
     # app/views/public/comments/destroy.js.erbを参照する
-    # redirect_to request.referer
+    redirect_to request.referer
   end
 
   private
