1cd5b061c1106bc5c116f8c6bbf0417ffc39c30c
YasugiMomo <yasugimomo.crw@gmail.com>
2022-05-28 12:12:44 +0000

    Add no-image

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,11 +2,12 @@ class Shop < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   has_one_attached :shop_image
+
   attr_accessor :average
 
   def get_shop_image(width, height)
     unless shop_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
       shop_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     shop_image.variant(resize_to_limit: [width, height]).processed
