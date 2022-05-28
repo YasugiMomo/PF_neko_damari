2c73ff816764dd65be038fd4b018355d717a9785
YasugiMomo <yasugimomo.crw@gmail.com>
2022-05-28 14:15:54 +0000

    Edit migrate

--- db/migrate/20220510174436_create_shops.rb
+++ db/migrate/20220510174436_create_shops.rb
@@ -1,4 +1,4 @@
-class CreateShops < ActiveRecord::Migration[6.0]
+class CreateShops < ActiveRecord::Migration[6.1]
   def change
     create_table :shops do |t|
       t.string :name, null: false
--- db/migrate/20220523105646_create_favorites.rb
+++ db/migrate/20220523105646_create_favorites.rb
@@ -1,4 +1,4 @@
-class CreateFavorites < ActiveRecord::Migration[6.0]
+class CreateFavorites < ActiveRecord::Migration[6.1]
   def change
     create_table :favorites do |t|
       t.integer :customer_id
