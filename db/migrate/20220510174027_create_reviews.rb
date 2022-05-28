ebbd905ecf3e1798ec8f1eabcd55b95477672ea7
YasugiMomo <yasugimomo.crw@gmail.com>
2022-05-28 15:32:55 +0000

    Edit migrate

--- db/migrate/20220510174027_create_reviews.rb
+++ db/migrate/20220510174027_create_reviews.rb
@@ -2,13 +2,14 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
       t.bigint :customer_id, limit: 20
-      t.bigint :shop_id, limit: 20
+      # t.bigint :shop_id, limit: 20
       t.string :title
       t.text :content
       t.float :rate, null: false, default: "0"
 
       t.timestamps
     end
-    add_foreign_key :customers, :shops
+    add_foreign_key :customers
+    # :shops
   end
 end
