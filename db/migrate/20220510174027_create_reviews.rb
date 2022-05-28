afff156ce715a81bd980d2813bc45dbeb9e917c5
YasugiMomo <yasugimomo.crw@gmail.com>
2022-05-28 14:34:25 +0000

    Edti migrate

--- db/migrate/20220510174027_create_reviews.rb
+++ db/migrate/20220510174027_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-      t.references :custoemr, type: :bigint, foreign_key: true
+      t.references :customer, type: :bigint, foreign_key: true
       t.references :shop, type: :bigint, foreign_key: true
       t.string :title
       t.text :content
--- db/migrate/20220523105646_create_favorites.rb
+++ db/migrate/20220523105646_create_favorites.rb
@@ -1,7 +1,7 @@
 class CreateFavorites < ActiveRecord::Migration[6.1]
   def change
     create_table :favorites do |t|
-      t.references :custoemr, type: :bigint, foreign_key: true
+      t.references :customer, type: :bigint, foreign_key: true
       t.references :review, type: :bigint, foreign_key: true
 
       t.timestamps
